3a341bfe66dfea9fa24d82668f6d0b6da3839b6a
Guilherme André Rebello <rebello.guilherme@gmail.com>
2022-05-21 23:28:15 -0300

    feat(Application) Implementa CQRS Adiciona as classes de command, query e handlers.

--- CleanArchMvc.Application/Interfaces/IProductService.cs
+++ CleanArchMvc.Application/Interfaces/IProductService.cs
@@ -1,8 +1,5 @@
 ﻿using CleanArchMvc.Application.DTOs;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace CleanArchMvc.Application.Interfaces
---
+++ CleanArchMvc.Application/Products/Commands/ProductRemoveCommand.cs
@@ -0,0 +1,14 @@
+﻿using CleanArchMvc.Domain.Entities;
+using MediatR;
+
+namespace CleanArchMvc.Application.Products.Commands
+{
+    public class ProductRemoveCommand : IRequest<Product> //herança de IRequest pois para remover um produto eu não preciso nada além do Id do produto
+    {
+        public int Id { get; set; }
+        public ProductRemoveCommand(int id)
+        {
+            Id = id;
+        }
+    }
+}
